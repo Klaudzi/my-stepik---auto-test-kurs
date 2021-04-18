5e8b0019b3e5d28917afa101544798d4c2a8f96b
yaliza <123yak12@gmail.com>
2021-04-18 20:29:38 +0300

    добавлен третий файл

---
+++ task2_3.py
@@ -0,0 +1,32 @@
+from selenium import webdriver
+import time
+import math
+
+def calc(x):
+  return str(math.log(abs(12*math.sin(int(x))))) 
+
+link = "http://suninjuly.github.io/get_attribute.html"
+
+try:
+    browser = webdriver.Chrome()
+    browser.get(link)
+    x_element = browser.find_element_by_id("treasure")
+    treasure_valuex = x_element.get_attribute("valuex")
+    print("value of image: ", treasure_valuex)
+    y = calc(treasure_valuex)
+    input1 = browser.find_element_by_id("answer")
+    input1.send_keys(y)
+    option1 = browser.find_element_by_id("robotCheckbox")
+    option1.click()
+    option2 = browser.find_element_by_id("robotsRule")
+    option2.click()
+    button = browser.find_element_by_tag_name("button")
+    button.click()
+
+finally:
+    # успеваем скопировать код за 30 секунд
+    time.sleep(10)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
+
+# не забываем оставить пустую строку в конце файла
