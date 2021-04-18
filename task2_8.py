86ff588ba76813c601ac27f1b1776f4bd0989174
yaliza <123yak12@gmail.com>
2021-04-18 19:45:52 +0300

    добавлен первый файл

---
+++ task2_8.py
@@ -0,0 +1,34 @@
+from selenium import webdriver
+import time
+import os
+
+try: 
+    link = "http://suninjuly.github.io/file_input.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    # Ваш код, который заполняет обязательные поля
+    input1 = browser.find_element_by_name("firstname")
+    input1.send_keys("Ivan")
+    input2 = browser.find_element_by_name("lastname")
+    input2.send_keys("Petrov")
+    input3 = browser.find_element_by_name("email")
+    input3.send_keys("yako07@inbox.ru")
+   
+
+    current_dir = os.path.abspath(os.path.dirname(__file__))    # получаем путь к директории текущего исполняемого файла 
+    file_path = os.path.join(current_dir, 'task2_6.py')           # добавляем к этому пути имя файла 
+    element = browser.find_element_by_name("file")
+    element.send_keys(file_path)
+ 
+    # Отправляем заполненную форму
+    button = browser.find_element_by_tag_name("button")
+    button.click()
+   
+finally:
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(10)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
+
+# не забываем оставить пустую строку в конце файла
